7f2a89bd9f6ac96aa9ea7859b56ddf5e9831480a
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-11-28 22:10:54 +0530

    feat: complete welcome screen

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SignUp = () => {
+  return (
+    <SafeAreaView>
+      <Text>Sign-up</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
---
+++ components/custom-button.tsx
@@ -0,0 +1,63 @@
+import { ButtonProps } from "@/types/type";
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+
+const getBgVarientStyle = (varient: ButtonProps["bgVariant"]) => {
+  switch (varient) {
+    case "secondary":
+      return "bg-gray-500";
+    case "danger":
+      return "bg-red-500";
+    case "success":
+      return "bg-green-500";
+    case "outline":
+      return "bg-transparent border-neutral-300 border-[0.5px]";
+
+    default:
+      return "bg-[#0286ff]";
+  }
+};
+const getTextVarientStyle = (varient: ButtonProps["textVariant"]) => {
+  switch (varient) {
+    case "primary":
+      return "bg-black";
+    case "secondary":
+      return "text-gray-100";
+    case "danger":
+      return "bg-red-100";
+    case "success":
+      return "bg-green-100";
+
+    default:
+      return "text-white";
+  }
+};
+
+const CustomButton = ({
+  onPress,
+  title,
+  bgVariant = "primary",
+  textVariant = "default",
+  IconLeft,
+  IconRight,
+  className,
+  ...props
+}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      className={`w-full rounded-full flex flex-row justify-center p-3 items-center shadow-md shadow-neutral-400/70 ${getBgVarientStyle(bgVariant)} ${className}`}
+      {...props}
+    >
+      {IconLeft && <IconLeft />}
+      <Text
+        className={`text-center text-lg font-bold ${getTextVarientStyle(textVariant)}`}
+      >
+        {title}
+      </Text>
+      {IconRight && <IconRight />}
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
